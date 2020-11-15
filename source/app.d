8346a0243de99280d437b31e79ca0b712d949427
Teashrock <kajitsu22@gmail.com>
2020-11-15 20:48:17 +0200

    Create app.d

---
+++ source/app.d
@@ -0,0 +1,13 @@
+import std.getopt;
+
+immutable {
+    outputFlagDefault = "messages.po";
+    outputFlagUsage   = "Write output to specified file (instead of `messages.po`).";
+}
+
+string outputFile;
+
+void main(string[] args)
+{
+    
+}
